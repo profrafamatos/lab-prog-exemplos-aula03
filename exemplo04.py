19296875d66fb3d22a86a9676df950a470b59359
Rafaella Matos <163598367+profrafamatos@users.noreply.github.com>
2026-02-26 11:56:06 +0000

    while com else

---
+++ exemplo04.py
@@ -0,0 +1,7 @@
+i = 1
+
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i não é menor que 6")
---
+++ microexercicio04.py
@@ -0,0 +1,7 @@
+pedido = 1
+
+while pedido < 5:
+    print(f'Separando pedido {pedido}')
+    pedido += 1
+else:
+    print("Todos os pedidos foram separados")
